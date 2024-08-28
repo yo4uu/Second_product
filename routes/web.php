7e82c618e2c4164d564027e9b04488bc4d795208
yo4yuu <yo4yuu@gmail.com>
2024-08-28 14:35:15 +0900

    ポートフォリオ用登録画面

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/welcome', function(){
+    return view('welcome');
+});
+
 Route::get('/', function () {
     return redirect('/login');
 });
